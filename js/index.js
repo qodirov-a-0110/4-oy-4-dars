c03dea43d4c56d9d7d5832bece43fec131ac34e2
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-05-24 13:43:36 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// 1-misol
+
+
+// 2-misol
+// let celse = +prompt('celse');
+// console.log(celse+273.15);
+// console.log(celse*1.80+32);
+
+
+// 3-masala
+// let vali = +prompt("v");
+// let ali =  +prompt("a");
+// let bil =  +prompt("b");
+// let g = (bil-vali);
+// let k = (ali-vali);
+// console.log(g);
+// console.log(k);
+
+// 4-masala
+// let n = +prompt("n");
+// let a =  +prompt("a");
+// let b = (n % a);
+// console.log(b);
+   
+// 5-masala
+// let n = +prompt("n");
+// let b = n * (n-2); 
+// console.log(b);
+
+
+// 7-masala
+// let b = +prompt("b");
+// let s =  +prompt("s");
+// let y = (b-s);
+// let h = (y/2)
+// console.log(h);
+
+// 8-masala
+// let b = +prompt("b");
+// let t =  +prompt("t");
+// let y = (b+t)/2;
+// console.log(y);
+
+// 9-masala
+// let n = +prompt("n");
+// let b = n * (n-3)/2; 
+// console.log(b);
